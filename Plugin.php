490e18587af9fe32257a8d0719fd1efd81103e05
jrotty <bssf@qq.com>
2026-01-22 20:55:56 +0800

    支持typecho1.3.0

--- Plugin.php
+++ Plugin.php
@@ -1,14 +1,25 @@
 <?php
+
+namespace TypechoPlugin\Titleshow;
+
+use Typecho\Plugin\PluginInterface;
+use Typecho\Widget\Helper\Form;
+use Typecho\Widget\Helper\Form\Element\Text;
+use Widget\Options;
+use Utils\Helper;
+use Typecho\Db;
+
+
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 /**
- * 该插件会让文章加密功能只加密文章内容！而不影响标题，标签还有评论数！【兼容情况：typecho1.1，开发板】
+ * 让文章显示标题
  * 
  * @package Titleshow
  * @author 泽泽
- * @version 1.1.2
+ * @version 1.1.3
  * @link https://github.com/jrotty/Titleshow
  */
-class Titleshow_Plugin implements Typecho_Plugin_Interface
+class Plugin implements PluginInterface
 {
     /**
      * 激活插件方法,如果激活失败,直接抛出异常
@@ -19,7 +30,7 @@ class Titleshow_Plugin implements Typecho_Plugin_Interface
      */
     public static function activate()
     {
-        Typecho_Plugin::factory('Widget_Abstract_Contents')->filter = array('Titleshow_Plugin', 'tshow');
+        \Typecho\Plugin::factory('Widget_Abstract_Contents')->title =  __CLASS__ . '::tshow';
     }
     
     /**
@@ -36,38 +47,21 @@ class Titleshow_Plugin implements Typecho_Plugin_Interface
      * 获取插件配置面板
      * 
      * @access public
-     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @param $form 配置面板
      * @return void
      */
-    public static function config(Typecho_Widget_Helper_Form $form)
+    public static function config($form)
     {
-?><style>@media (max-width: 767px){.yaofan {display: none!important;}}</style><?php
-      $say=array(
-        "看到下面那个大大的二维码了吗，想不想用你大大的手机扫扫它！",
-        "好几天没吃早饭了，打赏下开发者吧！",
-        "小伙子，插件好用么，打赏下作者好吗？",
-        "如果觉得好用，可以扫描下方二维码进行打赏，支持作者！",
-        "你知道吗，我特别喜欢听人民币到账的提示音！",
-        "听说，打赏我的人最后都找到了真爱。",
-        "打赏的都是天使。",
-        "打赏了的人都会变美~",
-        "打赏3块钱，帮我买杯肥宅快乐水，继续创作，谢谢大家！",
-        "阔乐，我想和大阔乐，就差3块钱了！",
-                         );
-        $tixing = new Typecho_Widget_Helper_Form_Element_Text('tixing', NULL, NULL, _t('密码文字提醒'), _t('不填写则默认为【请输入密码访问】<div class="yaofan"><br>
-        <b>作者 ❤ 语：'.$say[rand(0,9)].'</b><br><br><img src="'.Helper::options()->pluginUrl.'/Titleshow/yaofan.jpg" style="max-width: 100%;">
-        </div>'));
-        $form->addInput($tixing);
     }
     
     /**
      * 个人用户的配置面板
      * 
      * @access public
-     * @param Typecho_Widget_Helper_Form $form
+     * @param $form
      * @return void
      */
-    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+    public static function personalConfig($form){}
     
     /**
      * 插件实现方法
@@ -75,24 +69,14 @@ class Titleshow_Plugin implements Typecho_Plugin_Interface
      * @access public
      * @return void
      */
-public static function tshow($v, $obj) {
-$tixing = Typecho_Widget::widget('Widget_Options')->plugin('Titleshow')->tixing;//获取设置参数
-if(empty($tixing)){$tixing='请输入密码访问';} //如果未设置则设置默认文字
-$v['titleshow'] = false;
-/** 如果访问权限被禁止【就是如果需要密码】 */
-if ($v['hidden']){
-$v['text'] = '
-!!!
-<form class="protected" action="' . Typecho_Widget::widget('Widget_Security')->getTokenUrl($v['permalink']). '" method="post">'.'<p class="word">'.$tixing.'</p>'.'<p><input type="password" class="text" name="protectPassword" /><input type="hidden" name="protectCID" value="' . $v['cid'] . '" />&nbsp;<input type="submit" class="submit" value="' . _t('提交') . '" /></p>'.'</form>
-!!!
-';
-/** 跳过系统默认 */
-$v['hidden'] = false;
-/** 用于模板判断插件 */
-$v['titleshow'] = true;
-}
-/** 返回数据 */
-return $v; 
+public static function tshow($title, $obj) {
+    if($obj->hidden){
+        $db = Db::get();
+        return $db->fetchRow($db->select('title')->from('table.contents')->where('cid = ?',$obj->cid)->limit(1))['title'];
+    }else{
+        return $title;
+    }
+    
 }
 
 }
